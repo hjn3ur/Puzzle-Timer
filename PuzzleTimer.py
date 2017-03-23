81eec1b9ab1fe799087ecec202ff161dec1ae83f
Harrison <hjn3ur@virginia.edu>
2017-03-23 14:22:58 -0400

    Added a global queue to store the last 10 solve times. Created a function to add solve times to this queue immediately after they are computed. Started an analytics menu

--- PuzzleTimer.py
+++ PuzzleTimer.py
@@ -1,4 +1,8 @@
 import time
+import queue
+
+
+last_ten = queue.Queue(maxsize=10)
 
 
 def timer():
@@ -14,20 +18,45 @@ def timer():
         ending_time = time.time()
 
         solve_time = ending_time - starting_time
+        add_to_last_ten(solve_time)
         print("Solve time: %.3f seconds" % solve_time)
 
-        print("Record another time (y/n): ", end="")
+        print("Record another time? (y/n): ", end="")
         run_again = input()
 
         print()
 
-if __name__ == "__main__":
-    menu_option = ""
-    while menu_option != "0":
-        print("PuzzleTimer Menu \n0 - quit \n1 - Timer \nChoice: ", end="")
-        menu_option = input()
 
-        if menu_option == "1":
+def add_to_last_ten(solve_time):
+    global last_ten
+    if last_ten.full():
+        last_ten.get()
+    last_ten.put(solve_time)
+
+
+def print_last_ten_times():
+    global last_ten
+    print("Last ten solve times:")
+    for i in list(last_ten.queue):
+        print("%.3f" % i)
+
+
+def analytics_menu():
+    analytics_menu_option = ""
+    while analytics_menu_option != "0":
+        print("Analytics Menu \n0 - Back to main menu \n1 - Average of last ten solve times")
+
+
+if __name__ == "__main__":
+    main_menu_option = ""
+    while main_menu_option != "0":
+        print("PuzzleTimer Menu \n0 - Quit \n1 - Timer \n2 - Analytics \nChoice: ", end="")
+        main_menu_option = input()
+
+        if main_menu_option == "1":
             print()
             timer()
+        elif main_menu_option == "2":
+            print()
+            analytics_menu()
 
